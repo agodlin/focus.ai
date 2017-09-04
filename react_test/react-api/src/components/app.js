dea583b5e1f0c2a6b344a60a5428df047006e1e4
Arkady Godlin <arkady.godlin@gmail.com>
2017-09-04 22:05:19 +0300

    Learning Rest Api part 2

---
+++ react_test/react-api/src/components/app.js
@@ -0,0 +1,19 @@
+//=================
+// Import the dependencies that this component will need
+// to function properly
+//=================
+import React from "react";
+
+//=================
+// This is the component we'll be exporting into the
+// #app element on the index.html file.
+//=================
+export default class App extends React.Component {
+    render() {
+	return (
+	        <div>
+		Hello
+	    </div>
+	);
+    }
+}
